b34aca0e72b3acd61ef6ee87585231dc6e4aab5c
Vignan Kamarthi <147737851+vignankamarthi@users.noreply.github.com>
2024-03-13 11:28:59 -0400

    I completed the basics of React Tutorial. It went over React conceptually and its usefullness in web develop tment as well as how to build basic components. In the end I was able to make a button that shows an alert, and a close button for the alert.

---
+++ react-exercise/src/components/Button.tsx
@@ -0,0 +1,24 @@
+interface ButtonProps {
+  children: string;
+  color?:
+    | "primary"
+    | "secondary"
+    | "success"
+    | "danger"
+    | "warning"
+    | "info"
+    | "light"
+    | "dark"
+    | "link";
+  onClick: () => void;
+}
+
+const Button = ({ children, onClick, color = "primary" }: ButtonProps) => {
+  return (
+    <button className={"btn btn-" + color} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
